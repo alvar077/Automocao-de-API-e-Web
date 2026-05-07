cb69ae11dd95d7d1a851fe831137f9d89b70fe67
alvar077 <souzaalvaro348@gmail.com>
2026-05-06 21:26:10 -0300

    fix: adiciona espera explicita no CartPage

--- web-tests/pages/cart_page.py
+++ web-tests/pages/cart_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 
@@ -7,6 +8,8 @@ class CartPage(BasePage):
     CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def get_items_count(self):
+        # Espera pelo menos 1 item aparecer antes de contar
+        self.wait.until(EC.presence_of_element_located(self.CART_ITEMS))
         return len(self.driver.find_elements(*self.CART_ITEMS))
 
     def proceed_to_checkout(self):
